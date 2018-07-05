151bb482b60e4915b4d490db8be75436b8d6054b
박영호 <mellonia.bluerose@gmail.com>
2018-07-05 11:06:50 +0900

    make router

---
+++ router/main.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+    // router
+    app.get('/', (req, res) => {
+        res.render('index.html');
+    })
+
+    app.get('/about', (req, res) => {
+        res.render('about.html');
+    })
+}
