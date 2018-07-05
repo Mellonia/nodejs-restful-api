67dcdbb502c0efb99473d498250acd89fe3a139c
박영호 <mellonia.bluerose@gmail.com>
2018-07-05 21:12:24 +0900

    update express server

--- express.js
+++ express.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const router = require('./router/main.js')(app); // router module을 로드하여 app에 전달
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const fs = require('fs');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -11,3 +13,12 @@ const server =  app.listen(3000, () => {
 })
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+    secret: '@#@!$*$^!!@#&^', // 쿠키 변조 방지를 위한 sign 값
+    resave: false,  // 세션을 언제나 저장할지 정하는 값
+    saveUninitialized: true // 변경되지 않은 세션
+}));
+
+const router = require('./router/main.js')(app, fs); // router module을 로드하여 app에 전달
