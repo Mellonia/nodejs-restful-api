fe274f386810bb5f4f43618c323bff28e5d0aea3
박영호 <mellonia.bluerose@gmail.com>
2018-07-05 20:35:51 +0900

    file move

--- router/main.js
+++ router/main.js
@@ -1,8 +1,11 @@
-module.exports = (app) => {
+module.exports = (app, fs) => {
     // router
     app.get('/', (req, res) => {
-        res.render('index.html');
-    })
+        res.render('index', {
+            title: 'Index Page',
+            length: 5
+        })
+    });
 
     app.get('/about', (req, res) => {
         res.render('about.html');
