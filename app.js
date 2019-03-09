0347e35a6f2a5aceb6dff2e651640a98b8d655b2
박영호 <mellonia.bluerose@gmail.com>
2019-03-09 13:18:33 +0900

    set express server

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const UserRouter = require('./routes/user');
+
+// app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/users', UserRouter);
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000');
+});
