ecd07301f09b20a33b8fc4ff3b8233b752f313f5
박영호 <mellonia.bluerose@gmail.com>
2019-03-09 19:02:32 +0900

    update test code

--- spec/user.js
+++ spec/user.js
@@ -1,3 +1,5 @@
+const syncDatabase = require('../bin/sync-database');
+const models = require('../models/user');
 const should = require('should');
 const request = require('supertest');
 const app = require('../app');
@@ -5,6 +7,22 @@ const app = require('../app');
 // describe: 첫번째 파라미터로 테스트 수트 설명 문자열
 // it: 테스트 코트 작성 함수
 describe('GET /users', () => {
+    before('sync database', (done) => {
+        syncDatabase().then(() => {
+            done();
+        })
+    })
+
+    const users = [
+        { name: 'Elice' },
+        { name: 'Ryze' },
+        { name: 'Yasuo'}
+    ];
+
+    before('insert 3 users into database', (done) => {
+        models.User.bulkCreate(users).then(() => done());
+    });
+
     it('should return 200 status code', (done) => {
         request(app)
             .get('/users')
@@ -30,18 +48,22 @@ describe('GET /users', () => {
                 done();
             });
     });
+
+    // after('clear up database', (done) => {
+    //     syncDatabase().then(() => done());
+    // });
 });
 
-describe('PUT /users/:id', () => {
-    it.only('should return 200 status code', (done) => {
-        request(app)
-            .put('/users/1')
-            .send({
-                name: 'foo'
-            })
-            .end((err, res) => {
-                if (err) throw err;
-                done();
-            });
-    });
-});
+// describe('PUT /users/:id', () => {
+//     it.only('should return 200 status code', (done) => {
+//         request(app)
+//             .put('/users/1')
+//             .send({
+//                 name: 'foo'
+//             })
+//             .end((err, res) => {
+//                 if (err) throw err;
+//                 done();
+//             });
+//     });
+// });
