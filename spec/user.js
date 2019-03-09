723eca83763cd439d658c5b13fe5663916d884d9
박영호 <mellonia.bluerose@gmail.com>
2019-03-09 15:36:15 +0900

    write test code

---
+++ spec/user.js
@@ -0,0 +1,33 @@
+const should = require('should');
+const request = require('supertest');
+const app = require('../app');
+
+// describe: 첫번째 파라미터로 테스트 수트 설명 문자열
+// it: 테스트 코트 작성 함수
+describe('GET /users', () => {
+    it('should return 200 status code', (done) => {
+        request(app)
+            .get('/users')
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err;
+                done();
+            })
+    });
+
+    it('shoule return array', (done) => {
+        request(app)
+            .get('/users')
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err;
+                res.body.should.be.instanceof(Array).and.have.length(3);
+                res.body.map(user => {
+                    user.should.have.properties('id', 'name');
+                    user.id.should.be.a.Number();
+                    user.name.should.be.a.String();
+                });
+                done();
+            });
+    });
+});
