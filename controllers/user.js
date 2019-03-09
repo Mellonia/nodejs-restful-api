578e5e98157b5bc8597973a4c122ffdfc06493c9
박영호 <mellonia.bluerose@gmail.com>
2019-03-09 18:13:37 +0900

    add update function on controller, router

--- controllers/user.js
+++ controllers/user.js
@@ -1,20 +1,25 @@
-let users = [
-    {
-        id: 1,
-        name: 'alice'
-    },
-    {
-        id: 2,
-        name: 'yasuo'
-    },
-    {
-        id: 3,
-        name: 'ryze'
-    }
-]
+const models = require('../models/user');
+
+// let users = [
+//     {
+//         id: 1,
+//         name: 'alice'
+//     },
+//     {
+//         id: 2,
+//         name: 'yasuo'
+//     },
+//     {
+//         id: 3,
+//         name: 'ryze'
+//     }
+// ]
 
 getUsers = (req, res) => {
-    return res.json(users);
+    // return res.json(users);
+
+    models.User.findAll()
+        .then(users => res.json(users));
 }
 
 getUser = (req, res) => {
@@ -22,11 +27,23 @@ getUser = (req, res) => {
     if (!id) {
         return res.status(400).json({ error: 'Incorrect id' });
     }
-    let user = users.filter(user => user.id === id)[0]
-    if (!user) {
-        return res.status(400).json({ error: 'Unknown user' });
-    }
-    return res.json(user);
+
+    models.User.findOne({
+        where: {
+            id
+        }
+    }).then(user => {
+        if (!user) {
+            return res.status(404).json({ error: 'No user' });
+        }
+        return res.json(user);
+    });
+
+    // let user = users.filter(user => user.id === id)[0]
+    // if (!user) {
+    //     return res.status(400).json({ error: 'Unknown user' });
+    // }
+    // return res.json(user);
 }
 
 deleteUser = (req, res) => {
@@ -34,13 +51,20 @@ deleteUser = (req, res) => {
     if (!id) {
         return res.status(400).json({ error: 'Incorrect id' });
     }
-    const userIdx = users.findIndex(user => user.id === id);
-    if(userIdx === -1) {
-        return res.status(404).json({ error: 'Unknown user' });
-    }
 
-    users.splice(userIdx, 1);
-    res.status(204).send();
+    models.User.destroy({
+        where: {
+            id
+        }
+    }).then(() => res.status(204).send());
+
+    // const userIdx = users.findIndex(user => user.id === id);
+    // if(userIdx === -1) {
+    //     return res.status(404).json({ error: 'Unknown user' });
+    // }
+
+    // users.splice(userIdx, 1);
+    // res.status(204).send();
 }
 
 addUser = (req, res) => {
@@ -48,21 +72,49 @@ addUser = (req, res) => {
     if (!name.length) {
         return res.status(400).json({ error: 'Incorrect name' });
     }
-    const id = users.reduce((maxId, user) => {
-        return user.id > maxId ? user.id : maxId
-    }, 0) + 1;
 
-    const newUser = {
-        id,
+    models.User.create({
         name
+    }).then(user => {
+        res.status(201).json(user);
+    });
+
+    // const id = users.reduce((maxId, user) => {
+    //     return user.id > maxId ? user.id : maxId
+    // }, 0) + 1;
+
+    // const newUser = {
+    //     id,
+    //     name
+    // }
+    // users.push(newUser);
+    // return res.status(201).json(newUser);
+}
+
+updateUser = (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const name = req.body.name || '';
+    if (!id) {
+        return res.status(400).json({ error: 'Incorrect id' });
     }
-    users.push(newUser);
-    return res.status(201).json(newUser);
+    if (!name.length) {
+        return res.status(400).json({ error: 'Incorrect name' });
+    }
+
+    models.User.update({
+        name, 
+        where: {
+            id
+        }
+    }).then(user => {
+        return res.status(200).json(user);
+    })
 }
 
 module.exports = {
     getUsers,
     getUser,
     deleteUser,
-    addUser
+    addUser,
+    updateUser
 }
--- routes/user.js
+++ routes/user.js
@@ -10,4 +10,6 @@ router.delete('/:id', UserController.deleteUser);
 
 router.post('/', UserController.addUser);
 
+router.put('/:id', UserController.updateUser);
+
 module.exports = router;
